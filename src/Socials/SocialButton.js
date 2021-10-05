3d0991bcb56a3f06ab4b36e3205c82ccdc3bfb4f
Daksh Sethi <sethidaksh02@gmail.com>
2021-10-05 16:25:21 +0530

    Added Favicon + Logos

--- src/Socials/SocialButton.js
+++ src/Socials/SocialButton.js
@@ -1,7 +1,11 @@
+import Link from './../images/link.png'
+
 const SocialButton = (props) => {
     return (
         <a href={props.link} target="_blank" rel="noreferrer" className="button" style={{ background: `linear-gradient(145deg, ${props.bgfrom}, ${props.bgto})` }} >
+            <img src = { props.logo } alt="logo" />
             {props.name}
+            <img src = { Link } alt="link" style={{ width: '20px' }} />
         </a>
     );
 }
--- src/Socials/Socials.js
+++ src/Socials/Socials.js
@@ -1,13 +1,17 @@
 import SocialButton from './SocialButton';
+import github from './../images/github.svg';
+import instagram from './../images/instagram.svg';
+import twitter from './../images/twitter.svg';
+import linkedin from './../images/linkedin.svg';
 
 const Socials = () => {
     return (
         <div className="socials">
             <h3 style={{ width: '90%' }}>Get in touch... 🌐🤙</h3>
-            <SocialButton name="GitHub" bgfrom="#28bd69" bgto="#219f58" link="https://www.github.com/dakshsethi" />
-            <SocialButton name="Instagram" bgfrom="#e13b8e" bgto="#bd3278" link="https://www.instagram.com/dakshxsethi" />
-            <SocialButton name="Twitter" bgfrom="#3b8be1" bgto="#3275bd" link="https://www.twitter.com/sethidaksh02" />
-            <SocialButton name="LinkedIn" bgfrom="#1c5693" bgto="#17487b" link="https://www.linkedin.com/in/daksh-sethi/" />
+            <SocialButton name="GitHub" bgfrom="#28bd69" bgto="#219f58" logo={ github } link="https://www.github.com/dakshsethi" />
+            <SocialButton name="Instagram" bgfrom="#e13b8e" bgto="#bd3278" logo={ instagram } link="https://www.instagram.com/dakshxsethi" />
+            <SocialButton name="Twitter" bgfrom="#3b8be1" bgto="#3275bd" logo={ twitter } link="https://www.twitter.com/sethidaksh02" />
+            <SocialButton name="LinkedIn" bgfrom="#1c5693" bgto="#17487b" logo={ linkedin } link="https://www.linkedin.com/in/daksh-sethi/" />
         </div>
     );
 }
